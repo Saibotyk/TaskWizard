49f32e1b719f87755069d2f3df01b473cc9b774a
Léa Valmy <lea.valmy@gmail.com>
2023-06-27 16:10:40 +0200

    connecting database

--- index.php
+++ index.php
@@ -7,6 +7,20 @@
     <title>Document</title>
 </head>
 <body>
+   <?php
+   try {
+    $dbCo = new PDO('mysql:host=localhost;dbname=taskwizard;charset=utf8',
+    'root');
+    $dbCo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,
+    PDO::FETCH_ASSOC);
+   }
+
+   catch (Exception $e) {
+    die('Unable to connect to the database.
+    '.$e->getMessage());
+    }
+    
+   ?> 
     <script src="js/script.js"></script>
 </body>
 </html>
