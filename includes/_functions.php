097e4d0e2725e9b659976c4657cd217689518125
Léa Valmy <lea.valmy@gmail.com>
2023-06-27 21:21:23 +0200

    us 1 done, waiting for validation

--- includes/_functions.php
+++ includes/_functions.php
@@ -1,13 +1,25 @@
 <?php
 
-function getList( array $array): string
-        {
-            $html = "";
-            foreach ($array as $task) {
-                $html .= "<h2>" . $task['title'] . "</h2>";
-                $html .= "<p>" . $task['description'] . "</p>";
-            }
-            return $html;
-        }
-
-        ?>
+// function getList( array $array): string
+//         {
+//             $html = "";
+//             foreach ($array as $task) {
+//                 $html .= "<h2>" . $task['title'] . "</h2>";
+//                 $html .= "<p>" . $task['description'] . "</p>";
+//             }
+//             return $html;
+//         }
+        
+function getList(array $array) :string  {
+                    $html = '<ul class="list">';
+                    foreach ($array as $task) {
+                        if ($task['is_completed'] == 0){
+                            $img = 'img/checkbox.png" alt="checkbox"';
+                        }
+                        else{$img= 'img/checkbox_completed.png" alt="checkbox completed"';}
+                        $html .= '<li class="list-item"><img src="'.$img.'">';
+                        $html .= $task['description']."</li>";
+                    }
+                    $html .= '</ul>';
+                    return $html;
+                }
--- index.php
+++ index.php
@@ -1,30 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>TaskWizard</title>
-</head>
+<?php require 'includes/_head.php';
+require 'includes/_database.php';
+require 'includes/_functions.php';
+$query = $dbCo->prepare('SELECT * FROM task');
+$query->execute();
+$tasks = $query->fetchAll();
+?>
 
 <body>
+    <header class="header">
+        <h2>Mes tâches</h2>
+        <img src="img/plus-violet.png" alt="plus dans une case violette">
+    </header>
     <main>
-        <?php
-        require 'includes/_database.php';
-        require 'includes/_functions.php';
-        $query = $dbCo->prepare('SELECT * FROM task');
-        $query->execute();
-        $tasks = $query->fetchAll();
-        echo getList($tasks);
-        ?>
         <section>
-          <article class="container">
-            <h2 class="container__title">To do</h2>
-            <ul class="container__list">
-                <li class="container-list-itm"></li>
-            </ul>
-          </article>  
+            <article class="container">
+                <h3 class="container-subtitle">Aujourd'hui</h3>
+                <?=getList($tasks)?>
+                <!-- <ul> -->
+                <!-- <li class="list-item"><img src="img/checkbox.png" alt="checkbox">This is a task</li> -->
+                <!-- </ul> -->
+            </article>
         </section>
     </main>
     <script src="js/script.js"></script>
