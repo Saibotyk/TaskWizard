2d75b8a227ce9eeefb2117658e7028133c794007
Léa Valmy <lea.valmy@gmail.com>
2023-06-28 15:40:29 +0200

    us3 done validation ok

--- update.php
+++ update.php
@@ -4,12 +4,12 @@ require 'includes/_database.php';
 if (array_key_exists('id', $_GET)){
     $query = $dbCo->prepare("UPDATE task SET is_completed = 1 WHERE task.id_task=:id");
     $isOk = $query->execute(
-        ["id" => strip_tags(intval($_GET['id']))]
+        ["id" => intval(strip_tags($_GET['id']))]
     );
     if($_GET['is_completed'] == 1){
         $query = $dbCo->prepare("UPDATE task SET is_completed = 0 WHERE task.id_task=:id");
         $isOk = $query->execute(
-            ["id" => strip_tags(intval($_GET['id']))]
+            ["id" => intval(strip_tags($_GET['id']))]
         );
     };
 }
