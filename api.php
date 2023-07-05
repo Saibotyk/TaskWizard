cd7633765d44b69c8a70ec8329a7e44e4d7296a2
Léa Valmy <lea.valmy@gmail.com>
2023-07-05 16:26:56 +0200

    async working

--- api.php
+++ api.php
@@ -3,69 +3,86 @@ header('content-type:application/json');
 require "includes/_database.php";
 $data = json_decode(file_get_contents('php://input'), true);
 
+if ($data['action'] === 'move') {
+    $query2 = $dbCo->prepare('SELECT id_task, ranking, MAX(ranking) AS maxrank, MIN(ranking) AS minrank FROM task ORDER BY ranking');
+    $query2->execute();
+    $ranking = $query2->fetchAll();
+    //ca me brise les noisettes !
 
-$query2 = $dbCo->prepare('SELECT id_task, ranking, MAX(ranking) AS maxrank, MIN(ranking) AS minrank FROM task ORDER BY ranking');
-$query2->execute();
-$ranking = $query2->fetchAll();
- //ca me brise les noisettes !
-
-$maxRank = $ranking[0]['maxrank'];
-$minRank = $ranking[0]['minrank'];
+    $maxRank = $ranking[0]['maxrank'];
+    $minRank = $ranking[0]['minrank'];
 
 
 
-$rankingPlus = $data['ranking'] - 1;
-$ranking = intval($data['ranking']);
-$rankingMinus = $data['ranking'] + 1;
+    $rankingPlus = $data['ranking'] - 1;
+    $ranking = intval($data['ranking']);
+    $rankingMinus = $data['ranking'] + 1;
+    
+    if (($ranking == $maxRank && $data['prior'] == 'down') || ($ranking == $minRank && $data['prior'] == 'up')) {
+        echo json_encode([
+            "result" => false
+        ]);
+        exit;
+        
+    }
 
-if (($ranking == $maxRank && $data['prior'] == 'down') || ($ranking == $minRank && $data['prior'] == 'up')) {
+
+
+    if (array_key_exists('ranking', $data) && $data['prior'] == 'down') {
+        $query1 = $dbCo->prepare("UPDATE task SET ranking = ranking + 1 WHERE ranking=:rank");
+        $isOk = $query1->execute([
+            "rank" => intval(strip_tags($ranking))
+        ]);
+
+        $query2 = $dbCo->prepare("UPDATE task SET ranking = ranking - 1 WHERE ranking=:rankMinus AND NOT id_task=:id");
+        $isOk = $query2->execute([
+            "id" => intval(strip_tags($data['id'])),
+            "rankMinus" => intval(strip_tags($rankingMinus))
+        ]);
+
+        echo json_encode([
+            "result" => $isOk,
+            "id" => $data['id'],
+            "prior" => $data['prior']
+        ]);
+
+        exit;
+    } else if (array_key_exists('ranking', $data) && $data['prior'] == 'up') {
+        $query3 = $dbCo->prepare("UPDATE task SET ranking = ranking - 1 WHERE ranking=:rank");
+        $isOk = $query3->execute([
+            "rank" => intval(strip_tags($ranking))
+        ]);
+
+        $query4 = $dbCo->prepare("UPDATE task SET ranking = ranking + 1 WHERE ranking=:rankPlus AND NOT id_task=:id");
+        $isOk = $query4->execute([
+            "id" => intval(strip_tags($data['id'])),
+            "rankPlus" => intval(strip_tags($rankingPlus))
+        ]);
+
+        echo json_encode([
+            "result" => $isOk,
+            "id" => $data['id'],
+            "prior" => $data['prior']
+        ]);
+
+        exit;
+    }
+}
+// test async
+
+
+if ($data['action'] === 'rename' && $_SERVER['REQUEST_METHOD'] === 'PUT') {
+    $id = intval(strip_tags($data['idTask']));
+    $name = trim(strip_tags($data['taskName']));
+    $query = $dbCo->prepare("UPDATE `task` SET `title` = :articleName WHERE `id_task` = :idArticle;");
+    $isOk = $query->execute([
+        'idArticle' => $id,
+        'articleName' => $name
+    ]);
     echo json_encode([
-        "result" => false
+        'result' => $isOk && $query->rowCount() > 0,
+        'idArticle' => $id,
+        'articleName' => $name
     ]);
-    exit;
-} 
-
-
-
-if (array_key_exists('ranking', $data) && $data['prior'] == 'down') {
-    $query1 = $dbCo->prepare("UPDATE task SET ranking = ranking + 1 WHERE ranking=:rank");
-    $isOk = $query1->execute([
-        "rank" => intval(strip_tags($ranking))
-    ]);
-
-    $query2 = $dbCo->prepare("UPDATE task SET ranking = ranking - 1 WHERE ranking=:rankMinus AND NOT id_task=:id");
-    $isOk = $query2->execute([
-        "id" => intval(strip_tags($data['id'])),
-        "rankMinus" => intval(strip_tags($rankingMinus))
-    ]);
-
-    echo json_encode([
-        "result" => $isOk,
-        "id" => $data['id'],
-        "prior" => $data['prior']
-    ]);
-
-    exit;
-
-} else if (array_key_exists('ranking', $data) && $data['prior'] == 'up'){
-    $query3 = $dbCo->prepare("UPDATE task SET ranking = ranking - 1 WHERE ranking=:rank");
-    $isOk = $query3->execute([
-        "rank" => intval(strip_tags($ranking))
-    ]);
-
-    $query4 = $dbCo->prepare("UPDATE task SET ranking = ranking + 1 WHERE ranking=:rankPlus AND NOT id_task=:id");
-    $isOk = $query4->execute([
-        "id" => intval(strip_tags($data['id'])),
-        "rankPlus" => intval(strip_tags($rankingPlus))
-    ]);
-
-    echo json_encode([
-        "result" => $isOk,
-        "id" => $data['id'],
-        "prior" => $data['prior']
-    ]);
-
     exit;
 }
-
-?>
--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ button.addEventListener('click', function () {
 let popup = document.querySelector('.popup-js');
 let documentHtml = document.querySelector('html');
 
-documentHtml.addEventListener('click', function(event) {
+documentHtml.addEventListener('click', function (event) {
     if (event.target.classList.contains('popup-js')) {
         popup.classList.add('_display-none');
     }
@@ -27,17 +27,73 @@ const modifiers = document.querySelectorAll('.modifier-js');
 const listItems = document.querySelectorAll('.list-js');
 const inputId = document.querySelector('.input-js');
 const ttlsTask = document.querySelectorAll('.text-task-js');
+const ttlTask = document.querySelector('.text-task-js');
 const inputTtl = document.querySelector('.input-ttl-js');
 
 
 modifiers.forEach((modifier) => {
     modifier.addEventListener('click', function () {
         form.classList.toggle('_display-none');
-        inputId.setAttribute('value', this.parentElement.parentElement.dataset.id);
-        inputTtl.setAttribute('value', this.parentElement.parentElement.querySelector('.text-task-js').innerHTML);  
-        console.log(inputTtl) 
+        inputId.value = this.parentElement.parentElement.dataset.id;
+        inputTtl.value = this.parentElement.parentElement.querySelector('.text-task-js').innerText;
+        console.log(this.parentElement.parentElement.querySelector('.text-task-js').innerText);
     });
-}); 
+});
+
+//testing async
+
+const formModify = document.querySelector('.js-form');
+
+
+formModify.addEventListener('submit', e => {
+    e.preventDefault();
+    renameTask(inputId.value, inputTtl.value)
+        .then(apiResponse => {
+            if (!apiResponse.result) {
+                console.error('Erreur lors du renommage');
+                formModify.remove
+                return;
+            }
+            updateTaskName(inputId.value, inputTtl.value);
+            formModify.parentElement.classList.toggle('_display-none')
+        })
+
+}
+)
+
+
+async function callAPI(method, data) {
+    try {
+        const response = await fetch("api.php", {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        return response.json();
+    }
+    catch (error) { console.error("Error ") }
+    ;
+}
+
+
+function renameTask(idTask, taskName) {
+    const data = {
+        action: 'rename',
+        idTask: idTask,
+        taskName: taskName,
+        token: 'dfsfqf'
+    };
+
+    return callAPI('PUT', data);
+}
+
+function updateTaskName(id, name){
+    document.querySelector(`[data-id="${id}"] .text-task-js`).innerText = name;
+}
+
+
 
 // ranking tasks
 
@@ -47,12 +103,13 @@ const buttons = document.querySelectorAll('.js-btn');
 buttons.forEach((button) => {
     button.addEventListener('click', e => {
         let object = {
-            id : button.dataset.id,
-            ranking : button.dataset.ranking,
-            prior : button.dataset.prior
+            action: 'move',
+            id: button.dataset.id,
+            ranking: button.dataset.ranking,
+            prior: button.dataset.prior
         }
         moveTask(object)
-        .then(apiResponse => console.table(apiResponse));
+            .then(apiResponse => console.table(apiResponse));
     });
 });
 
@@ -72,3 +129,4 @@ async function moveTask(data) {
     }
 }
 
+// async test 
